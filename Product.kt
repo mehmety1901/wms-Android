c1405723628532c5a13c4b5dc6747f602336c8e3
mehmety1901 <76120544+mehmety1901@users.noreply.github.com>
2025-10-03 11:21:58 +0300

    Add Product data class with relevant fields

--- Product.kt
+++ Product.kt
@@ -1,1 +1,10 @@
+data class Product(
+    val sku: String,
+    val name: String,
+    val quantity: Int,
+    val rackCode: String,
+    val rackSlot: Int,
+    val packageType: String,
+    val isGift: Boolean
+)
 
